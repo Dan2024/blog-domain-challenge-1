27258dcf0d811cb4e142a7acd90c8ebf5ca3fbf8
Dan <dannorthcott8@gmail.com>
2022-06-08 15:15:10 +0100

    requirements done

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,47 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+  const createdUser = await prisma.user.create({
+    data: { username: "alice1", email: "alice1@gmail.com" },
+  });
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+  const createdProfile = await prisma.profile.create({
+    data: {
+      profPicUrl: "https:/alice1profilepic.com",
+      bio: "alice1 test bio",
+      userId: createdUser.id,
+    },
+  });
 
-    // Add your code here
+  const createdPost = await prisma.post.create({
+    data: {
+      title: "alice test post title 1",
+      content: "alice test post 1",
+      //   published: true,
+      //   pictureUrl: 'https:/alicetestpostpicture1.com'
+      userId: createdUser.id,
+    },
+  });
 
-    
+  const createdComment = await prisma.comment.create({
+    data: {
+      content: "test comment 1 on alice test post 1 ",
+      postId: createdPost.id,
+      userId: createdUser.id,
+    },
+  });
 
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  console.log(`user created`, createdUser);
+  console.log(`profile created`, createdProfile);
+  console.log(`Post created`, createdPost);
+  console.log(`Comment created`, createdComment);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
